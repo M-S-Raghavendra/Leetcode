0fe2ccc6be270eeea490233468b86fb4edef71b6
M-S-Raghavendra <99091196+M-S-Raghavendra@users.noreply.github.com>
2022-12-23 01:05:09 +0530

    Time: 3 ms (98.04%), Space: 7.1 MB (46.88%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,22 +1,33 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        unordered_map<char,char> m;
-        unordered_set<char> set;
+//         unordered_map<char,char> m;
+//         unordered_set<char> set;
         
-        for(int i=0;i<s.length();i++)
+//         for(int i=0;i<s.length();i++)
+//         {
+//             if(m.find(s[i]) != m.end())
+//             {
+//                 if(m[s[i]] != t[i])
+//                     return false;
+//             }
+//             else {
+//                 if(set.find(t[i]) != set.end())
+//                     return false;
+//                 m[s[i]] = t[i];
+//                 set.insert(t[i]);
+//             }
+//         }
+        
+        //Simple solution
+        int n = s.length();
+        vector<int> m1(256,-1), m2(256,-1);
+        
+        for(int i=0;i<n;i++)
         {
-            if(m.find(s[i]) != m.end())
-            {
-                if(m[s[i]] != t[i])
-                    return false;
-            }
-            else {
-                if(set.find(t[i]) != set.end())
-                    return false;
-                m[s[i]] = t[i];
-                set.insert(t[i]);
-            }
+            if(m1[s[i]] != m2[t[i]]) return false;
+            m1[s[i]] = i;
+            m2[t[i]] = i;
         }
         
         return true;
