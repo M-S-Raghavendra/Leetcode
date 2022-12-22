cd3da35358c21c25004b372d76d7a7b2e646c043
M-S-Raghavendra <99091196+M-S-Raghavendra@users.noreply.github.com>
2022-12-22 11:54:42 +0530

    Time: 52 ms (45.07%), Space: 18.4 MB (48.63%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -3,7 +3,27 @@ public:
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
         vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-        return f(n-1,amount,coins,n,dp);
+        // return f(n-1,amount,coins,n,dp);
+        
+        for(int val=0;val<=amount;val++)
+        {
+            if(val%coins[0] == 0) dp[0][val] = 1;
+            else dp[0][val] = 0;
+        }
+        
+        for(int ind = 1;ind<n;ind++)    
+        {
+            for(int val=0;val<=amount;val++)
+            {
+                int nottake = dp[ind-1][val];
+                int take = 0;
+                if(coins[ind] <= val)
+                    take = dp[ind][val-coins[ind]];
+                dp[ind][val] = (take + nottake);
+            }
+        }
+        
+        return dp[n-1][amount];
     }
     
     int f(int ind, int val, vector<int> &deno, int n, vector<vector<int>> &dp)
