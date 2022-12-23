52a875a1db2854fea1d461d4f9de9e7e0e97cb21
M-S-Raghavendra <99091196+M-S-Raghavendra@users.noreply.github.com>
2022-12-23 09:52:44 +0530

    Time: 88 ms (5.16%), Space: 18.8 MB (9.98%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,10 +1,29 @@
 class Solution {
 public:
-    int longestCommonSubsequence(string text1, string text2) {
-        int n = text1.length();
-        int m = text2.length();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return f(n-1,m-1,text1,text2,dp);
+    int longestCommonSubsequence(string s1, string s2) {
+        int n = s1.length();
+        int m = s2.length();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        
+        for(int ind1=0;ind1<=n;ind1++)
+        {
+            for(int ind2=0;ind2<=m;ind2++)
+            {
+                if(ind1==0 or ind2==0)
+                    dp[ind1][ind2] = 0;
+                else
+                {
+                    if(s1[ind1-1] == s2[ind2-1])
+                        dp[ind1][ind2] = 1 + dp[ind1-1][ind2-1];
+                    else
+                        dp[ind1][ind2] = 0 + max(dp[ind1-1][ind2], dp[ind1][ind2-1]);
+                }
+            }
+        }
+        
+        return dp[n][m];
+        
+        // return f(n-1,m-1,s1,s2,dp);
     }
     
     int f(int ind1, int ind2, string &s1, string &s2, vector<vector<int>> &dp)
