737d51553e4870fb45fc1e683daa7be1d7118cd4
M-S-Raghavendra <99091196+M-S-Raghavendra@users.noreply.github.com>
2023-01-04 12:33:35 +0530

    Time: 171 ms (20.28%), Space: 28.5 MB (21.10%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int minInsertions(string s) {
+        return s.length() - longestPalindromeSubseq(s);
+    }
+    
+    int longestPalindromeSubseq(string s) 
+    {
+        int n = s.length();
+        string t = s;
+        
+        //Longest Palindromic subsequence is the Longest Common subsequence(LCS)
+        //of s and rev(s)
+        reverse(t.begin(),t.end());     
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(s[i-1] == t[j-1])
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                else 
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        
+        return dp[n][n];        
+    }
+};
