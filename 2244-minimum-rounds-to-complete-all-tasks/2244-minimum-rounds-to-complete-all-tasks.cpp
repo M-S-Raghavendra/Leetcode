4c25e0446c284f84ae1ab5894d1737bd446e0284
M-S-Raghavendra <99091196+M-S-Raghavendra@users.noreply.github.com>
2023-01-04 08:56:10 +0530

    Time: 337 ms (55.68%), Space: 103.7 MB (45.14%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -10,18 +10,19 @@ public:
             int freq = x.second;
             if(freq == 1)
                 return -1;
-            if(freq%3 == 0)
-                ans += freq/3;
-            else if(freq%3 == 1)
-            {
-                ans += (freq/3) - 1;
-                ans += 2;
-            }
-            else if(freq%3 == 2)
-            {
-                ans += freq/3;
-                ans += 1;
-            }
+            ans += (freq + 2)/3;
+            // if(freq%3 == 0)
+            //     ans += freq/3;
+            // else if(freq%3 == 1)
+            // {
+            //     ans += (freq/3) - 1;
+            //     ans += 2;
+            // }
+            // else if(freq%3 == 2)
+            // {
+            //     ans += freq/3;
+            //     ans += 1;
+            // }
         }
         
         return ans;
