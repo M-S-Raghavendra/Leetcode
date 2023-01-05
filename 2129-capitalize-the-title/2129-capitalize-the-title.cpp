f3b80aba18a2eed68f7b7f054b6c4db492f2ee99
M-S-Raghavendra <99091196+M-S-Raghavendra@users.noreply.github.com>
2023-01-05 18:48:52 +0530

    Time: 4 ms (48.83%), Space: 6.2 MB (92.65%) - LeetHub

--- 2129-capitalize-the-title/2129-capitalize-the-title.cpp
+++ 2129-capitalize-the-title/2129-capitalize-the-title.cpp
@@ -3,32 +3,38 @@ public:
     string capitalizeTitle(string title) {
         int n = title.length();
         int s = 0, e = 0;
-        string ans = "";
+        // string ans = "";
         
         while(s < n and e < n)
         {
             while(e < n and title[e] != ' ')
+            {
+                title[e] = tolower(title[e]);
                 e++;
+            }
             
             int l = e-s;
-            
-            if(l == 1 or l == 2)
+            if(l > 2)            
             {
-                string str = title.substr(s,l);
-                ans += lower(str);
+                title[s] = toupper(title[s]);
             }
-            else {
-                string str = title.substr(s+1,l-1);
-                ans += toupper(title[s]);
-                ans += lower(str);
-            }
-            ans += ' ';
+            
+            // if(l == 1 or l == 2)
+            // {
+            //     string str = title.substr(s,l);
+            //     ans += lower(str);
+            // }
+            // else {
+            //     string str = title.substr(s+1,l-1);
+            //     ans += toupper(title[s]);
+            //     ans += lower(str);
+            // }
+            // ans += ' ';
             e++;
             s = e;
-            // cout<<s<<" "<<e<<"\n";
         }
-        ans.pop_back();
-        return ans;
+        // ans.pop_back();
+        return title;
     }
     
     string lower(string s)
