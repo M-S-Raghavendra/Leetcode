cac5fccfdb4ba1e23cea734e2465d72ecf28a7fe
M-S-Raghavendra <99091196+M-S-Raghavendra@users.noreply.github.com>
2023-01-20 14:08:29 +0530

    Time: 529 ms (5.01%), Space: 80.7 MB (5.06%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -3,13 +3,42 @@ public:
     bool isInterleave(string s1, string s2, string s3) {
         int n = s1.size();
         int m = s2.size();
-        int k = s3.size();
+        int l = s3.size();
         
         if(s1.empty()) return (s2 == s3);
         if(s2.empty()) return (s1 == s3);
         
-        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(m+1,vector<int>(k+1,-1)));
-        return f(n-1,m-1,k-1,s1,s2,s3,dp);
+        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(m+1,vector<int>(l+1,-1)));
+        
+        for(int i=0;i<=n;i++)
+        {
+            for(int j=0;j<=m;j++)
+            {
+                for(int k=0;k<=l;k++)
+                {
+                    if(i == 0 and j == 0 and k == 0)
+                        dp[i][j][k] = 1;
+                    else if(i == 0 and j == 0 and k != 0)
+                        dp[i][j][k] = 0;
+                    else {
+                        bool a = false, b = false;
+                        if(i >= 1 and k >= 1 and s1[i-1] == s3[k-1])
+                            a = dp[i-1][j][k-1];
+                        
+                        dp[i][j][k] = (a or b);
+                        
+                        if(j >= 1 and k >= 1 and s2[j-1] == s3[k-1])
+                            b = dp[i][j-1][k-1];
+                        
+                        dp[i][j][k] = (a or b);
+                    }
+                }
+            }
+        }
+        
+        return dp[n][m][l];
+        
+        // return f(n-1,m-1,l-1,s1,s2,s3,dp);
     }
     
     bool f(int i, int j, int k, string &s1, string &s2, string &s3, vector<vector<vector<int>>> &dp)
