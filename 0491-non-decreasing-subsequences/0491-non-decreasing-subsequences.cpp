347ba8d1361dbfed2447fd77bbb281bf19a24a06
M-S-Raghavendra <99091196+M-S-Raghavendra@users.noreply.github.com>
2023-01-20 13:30:10 +0530

    Time: 119 ms (38.75%), Space: 47.6 MB (22.00%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
@@ -1,54 +1,31 @@
-class Solution {
-private:
-    struct hashFunction 
-    {
-      size_t operator()(const vector<int> 
-                        &myVector) const 
-      {
-        std::hash<int> hasher;
-        size_t answer = 0;
-
-        for (int i : myVector) 
-        {
-          answer ^= hasher(i) + 0x9e3779b9 + 
-                    (answer << 6) + (answer >> 2);
-        }
-        return answer;
-      }
-    };
-    
+class Solution {    
 public:
     vector<vector<int>> findSubsequences(vector<int>& nums) {
-        unordered_set<vector<int>,hashFunction> s;
+        set<vector<int>> s;
         vector<int> v;
         
-        f(nums.size()-1,nums,v,s);
+        f(0,nums,v,s);
         vector<vector<int>> ans;
         for(auto x:s)
             ans.push_back(x);
         return ans;
     }
     
-    void f(int ind, vector<int> &nums, vector<int> &v, unordered_set<vector<int>,hashFunction> &s)
+    void f(int ind, vector<int> &nums, vector<int> &v, set<vector<int>> &s)
     {
-        if(ind < 0)
+        if(ind >= nums.size())
         {
             if(v.size() > 1)
-            {
-                vector<int> t;
-                for(int i=v.size()-1;i>=0;i--)
-                    t.push_back(v[i]);
-                s.insert(t);
-            }
+                s.insert(v);
             return ;
         }
         
-        f(ind-1,nums,v,s);
+        f(ind+1,nums,v,s);
         
-        if(v.empty() or nums[ind] <= v.back())
+        if(v.empty() or nums[ind] >= v.back())
         {
             v.push_back(nums[ind]);
-            f(ind-1,nums,v,s);
+            f(ind+1,nums,v,s);
             v.pop_back();
         }
     }
