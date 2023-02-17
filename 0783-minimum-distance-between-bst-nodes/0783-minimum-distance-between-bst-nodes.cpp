790712ad9902a473237888b4c25d11176ca2bd03
M-S-Raghavendra <99091196+M-S-Raghavendra@users.noreply.github.com>
2023-02-17 08:50:40 +0530

    Time: 4 ms (56.01%), Space: 9.7 MB (69.86%) - LeetHub

--- 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.cpp
+++ 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.cpp
@@ -11,32 +11,43 @@
  */
 class Solution {
 public:
+    int ans = INT_MAX, prev = -1;
+    
     int minDiffInBST(TreeNode* root) {
-        int ans = INT_MAX;
-        f(root,ans);
+        return f(root);
+        // return ans;
+    }
+    
+    int f(TreeNode* root)
+    {
+        if(root->left != NULL) f(root->left);
+        if(prev >= 0)
+            ans = min(ans,abs(root->val-prev));
+        prev = root->val;
+        if(root->right != NULL) f(root->right);
         return ans;
     }
     
-    int f(TreeNode* root, int &ans)
-    {
-        if(root)
-        {
-            sub(root->left,ans,root->val);
-            sub(root->right,ans,root->val);
-            f(root->left,ans);
-            f(root->right,ans);
-        }
-        return 1e9;
-    }
+//     int f(TreeNode* root, int &ans)
+//     {
+//         if(root)
+//         {
+//             sub(root->left,ans,root->val);
+//             sub(root->right,ans,root->val);
+//             f(root->left,ans);
+//             f(root->right,ans);
+//         }
+//         return 1e9;
+//     }
     
-    int sub(TreeNode* root, int &ans, int &top)
-    {
-        if(root)
-        {
-            ans = min(ans,abs((root->val)-top));
-            sub(root->left,ans,top);
-            sub(root->right,ans,top);
-        }
-        return 1e9;
-    }
+//     int sub(TreeNode* root, int &ans, int &top)
+//     {
+//         if(root)
+//         {
+//             ans = min(ans,abs((root->val)-top));
+//             sub(root->left,ans,top);
+//             sub(root->right,ans,top);
+//         }
+//         return 1e9;
+//     }
 };
