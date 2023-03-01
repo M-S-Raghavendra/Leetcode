035205392d7029ca9463f312b9d334fb96a32212
M-S-Raghavendra <99091196+M-S-Raghavendra@users.noreply.github.com>
2023-03-01 12:46:04 +0530

    Time: 153 ms (99.59%), Space: 84.8 MB (78.53%) - LeetHub

--- 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.cpp
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.cpp
@@ -4,27 +4,42 @@ public:
         int n = capacity.size();
         int ans = 0;
         
-        priority_queue<int, vector<int>, greater<int>> pq;
         for(int i=0;i<n;i++)
         {
-            if(capacity[i] == rocks[i])
-                ans++;
-            else
-            {
-                int diff = capacity[i] - rocks[i];
-                pq.push(diff);
-            }
+            rocks[i] = capacity[i] - rocks[i];
         }
         
-        while(!pq.empty() and additionalRocks > 0)
+        sort(rocks.begin(),rocks.end());
+        int i = 0;
+        
+        while(i < n and additionalRocks >= rocks[i])
         {
-            int top = pq.top();
-            pq.pop();
-            
-            additionalRocks -= top;
-            if(additionalRocks >= 0)
-                ans++;
+            ans++;
+            additionalRocks -= rocks[i++];
         }
+        
         return ans;
+//         priority_queue<int, vector<int>, greater<int>> pq;
+//         for(int i=0;i<n;i++)
+//         {
+//             if(capacity[i] == rocks[i])
+//                 ans++;
+//             else
+//             {
+//                 int diff = capacity[i] - rocks[i];
+//                 pq.push(diff);
+//             }
+//         }
+        
+//         while(!pq.empty() and additionalRocks > 0)
+//         {
+//             int top = pq.top();
+//             pq.pop();
+            
+//             additionalRocks -= top;
+//             if(additionalRocks >= 0)
+//                 ans++;
+//         }
+//         return ans;
     }
 };
