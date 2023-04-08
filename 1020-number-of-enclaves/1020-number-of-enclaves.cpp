e7b2a6303514115cc43d0dcb6396ab64d2cfa09f
M-S-Raghavendra <99091196+M-S-Raghavendra@users.noreply.github.com>
2023-04-08 16:07:20 +0530

    Time: 107 ms (12.98%), Space: 47.7 MB (7.84%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -0,0 +1,64 @@
+class Solution {
+private:
+    int d[5] = {0,1,0,-1,0};
+public:
+    int numEnclaves(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n  =grid[0].size();
+        
+        vector<vector<bool>> vis(m,vector<bool>(n,false));
+        int ans = 0;
+        
+        for(int i = 0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(grid[i][j] == 1 and !vis[i][j])
+                {
+                    ans += bfs(i,j,grid,vis);
+                }
+            }
+        }
+        
+        return ans;
+    }
+    
+    int bfs(int i, int j, vector<vector<int>> &g, vector<vector<bool>> &v)
+    {
+        queue<pair<int,int>> q;
+        q.push({i,j});
+        v[i][j] = true;
+        int ans = 0, cnt = 0;
+        bool bound =  false;
+        
+        while(!q.empty())
+        {
+            auto u = q.front();
+            q.pop();
+            cnt++;
+            
+            for(int k=0;k<4;k++)
+            {
+                int nx = u.first + d[k];
+                int ny = u.second + d[k+1];
+                
+                if(nx<0 or ny<0 or nx >= g.size() or ny >= g[0].size())
+                {
+                    bound = true;
+                    continue;
+                }
+                
+                if(g[nx][ny] == 1 and !v[nx][ny])
+                {
+                    q.push({nx,ny});
+                    v[nx][ny] = true;
+                }
+            }
+        }
+        
+        if(bound == false)
+            ans += cnt;
+        
+        return ans;
+    }
+};
