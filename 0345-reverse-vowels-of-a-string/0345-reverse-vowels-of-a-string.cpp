421e9b82ba1d72a5f3a83239608eda95efb8103e
M-S-Raghavendra <99091196+M-S-Raghavendra@users.noreply.github.com>
2023-05-18 08:44:13 +0530

    Time: 6 ms (72.14%), Space: 7.6 MB (85.68%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -9,20 +9,33 @@ private:
     
 public:
     string reverseVowels(string s) {
-        stack<char> stk;
-        for(auto x:s)
-            if(isVowel(x))
-                stk.push(x);
+        int n = s.length();
+        int i = 0, j = n-1;
         
-        for(int i=0;i<s.length();i++)
+        while(i < j)
         {
-            if(isVowel(s[i]))
-            {
-                s[i] = stk.top();
-                stk.pop();
-            }
+            while(i<n and !isVowel(s[i])) i++;
+            while(j>=0 and !isVowel(s[j])) j--;
+            if(i < j)
+                swap(s[i++],s[j--]);
         }
         
         return s;
+        
+//         stack<char> stk;
+//         for(auto x:s)
+//             if(isVowel(x))
+//                 stk.push(x);
+        
+//         for(int i=0;i<s.length();i++)
+//         {
+//             if(isVowel(s[i]))
+//             {
+//                 s[i] = stk.top();
+//                 stk.pop();
+//             }
+//         }
+        
+//         return s;
     }
 };
