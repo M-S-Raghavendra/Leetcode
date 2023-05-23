98b9e65c4779f7999fa3c605e43e10e0fbd1297f
M-S-Raghavendra <99091196+M-S-Raghavendra@users.noreply.github.com>
2023-05-23 08:46:29 +0530

    Time: 38 ms (50.69%), Space: 19.9 MB (60.44%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
@@ -6,35 +6,46 @@ private:
 public:
     KthLargest(int k, vector<int>& nums) {
         kk = k;
-        int i = 0;
-        int n = nums.size();
-        while(k-- and i < n)
-            pq.push(nums[i++]);
-        while(i < n) 
+        for(int i=0;i<nums.size();i++)
         {
-            if(nums[i] > pq.top())
-            {
+            pq.push(nums[i]);
+            if(pq.size() > k)
                 pq.pop();
-                pq.push(nums[i]);
-            }
-            i++;
         }
+        // int i = 0;
+        // int n = nums.size();
+        // while(k-- and i < n)
+        //     pq.push(nums[i++]);
+        // while(i < n) 
+        // {
+        //     if(nums[i] > pq.top())
+        //     {
+        //         pq.pop();
+        //         pq.push(nums[i]);
+        //     }
+        //     i++;
+        // }
     }
     
     int add(int val) {
-        if(pq.size() < kk)
-        {
-            pq.push(val);
-            return pq.top();
-        }
-        
-        if(val > pq.top())
-        {
+        pq.push(val);
+        if(pq.size() > kk)
             pq.pop();
-            pq.push(val);
-        }
-        
         return pq.top();
+        
+//         if(pq.size() < kk)
+//         {
+//             pq.push(val);
+//             return pq.top();
+//         }
+        
+//         if(val > pq.top())
+//         {
+//             pq.pop();
+//             pq.push(val);
+//         }
+        
+//         return pq.top();
     }
 };
 
