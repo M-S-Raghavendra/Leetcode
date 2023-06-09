d801b79c3bb346b778f8440636ac32d4a588c424
M-S-Raghavendra <99091196+M-S-Raghavendra@users.noreply.github.com>
2023-06-09 13:03:49 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -3,40 +3,103 @@
 using namespace std;
 
 // } Driver Code Ends
+
+class DisjointSet {
+    vector<int> size, par;
+public:
+    DisjointSet(int n) {
+        size.resize(n+1,1);
+        par.resize(n+1);
+        for(int i=0;i<=n;i++)
+            par[i] = i;
+    }
+    
+    int findUPar(int u) {
+        if(par[u] == u)
+            return u;
+        return par[u] = findUPar(par[u]);
+    }
+    
+    void unionBySize(int u, int v) {
+        int upu = findUPar(u);
+        int upv = findUPar(v);
+        if(upu == upv)
+            return;
+        if(size[upu] < size[upv]) {
+            par[upu] = upv;
+            size[upv] += size[upu];
+        }
+        else {
+            par[upv] = upu;
+            size[upu] += size[upv];
+        }
+    }
+};
+
 class Solution
 {
 	public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
     int spanningTree(int V, vector<vector<int>> adj[])
     {
-        typedef pair<int,int> pp;
-        int sum = 0;
-        priority_queue<pp,vector<pp>,greater<pp>> pq;
-        pq.push({0,0});
-        vector<int> included(V,0);
+        // // Prim's Algorithm
+        // typedef pair<int,int> pp;
+        // int sum = 0;
+        // priority_queue<pp,vector<pp>,greater<pp>> pq;
+        // pq.push({0,0});
+        // vector<int> included(V,0);
         
-        while(!pq.empty())
-        {
-            auto it = pq.top();
-            pq.pop();
-            int edgeW = it.first;
-            int node = it.second;
+        // while(!pq.empty())
+        // {
+        //     auto it = pq.top();
+        //     pq.pop();
+        //     int edgeW = it.first;
+        //     int node = it.second;
             
-            if(included[node])
-                continue;
+        //     if(included[node])
+        //         continue;
             
-            included[node] = 1;
-            sum += edgeW;
+        //     included[node] = 1;
+        //     sum += edgeW;
             
-            for(auto x:adj[node])
-            {
-                int adjNode = x[0];
-                int adjEdgeW = x[1];
+        //     for(auto x:adj[node])
+        //     {
+        //         int adjNode = x[0];
+        //         int adjEdgeW = x[1];
                 
-                if(!included[adjNode])
-                {
-                    pq.push({adjEdgeW,adjNode});
-                }
+        //         if(!included[adjNode])
+        //         {
+        //             pq.push({adjEdgeW,adjNode});
+        //         }
+        //     }
+        // }
+        
+        // return sum;
+        
+        // Kruskal's Algorithm
+        vector<pair<int,pair<int,int>>> edges;
+        for(int i=0;i<V;i++)
+        {
+            for(y:adj[i])
+            {
+                edges.push_back({y[1],{i,y[0]}});
+            }
+        }
+        
+        sort(edges.begin(),edges.end());
+        
+        DisjointSet ds(V);
+        int sum = 0;
+        for(auto x:edges)
+        {
+            int w = x.first;
+            int node = x.second.first;
+            int adjNode = x.second.second;
+            
+            if(ds.findUPar(node) != ds.findUPar(adjNode))
+            {
+                sum += w;
+                ds.unionBySize(node,adjNode);
             }
         }
         
