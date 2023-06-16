a2c5c06f655d4d80233f336a3dae35c7c0edb77e
M-S-Raghavendra <99091196+M-S-Raghavendra@users.noreply.github.com>
2023-06-16 09:45:42 +0530

    Time: 6 ms (23.95%), Space: 8.4 MB (36.31%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -1,27 +1,41 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        int index = -1;
         int n = nums.size();
-        for(int i=1;i<n;i++)
+        int cnt = 0;
+        
+        for(int i=0;i<n;i++)
         {
-            if(nums[i] < nums[i-1]) {
-                index = i;
-                break;
+            if(nums[i] > nums[(i+1)%n])
+            {
+                cnt++;
+                if(cnt > 1)
+                    return false;
             }
         }
-        
-        if(index == -1)
-            return true;
-        reverse(nums.begin(),nums.begin()+index);
-        reverse(nums.begin()+index,nums.end());
-        reverse(nums.begin(),nums.end());
-        
-        for(int i=1;i<n;i++)
-        {
-            if(nums[i] < nums[i-1])
-                return false;
-        }
         return true;
+        
+//         int index = -1;
+//         int n = nums.size();
+//         for(int i=1;i<n;i++)
+//         {
+//             if(nums[i] < nums[i-1]) {
+//                 index = i;
+//                 break;
+//             }
+//         }
+        
+//         if(index == -1)
+//             return true;
+//         reverse(nums.begin(),nums.begin()+index);
+//         reverse(nums.begin()+index,nums.end());
+//         reverse(nums.begin(),nums.end());
+        
+//         for(int i=1;i<n;i++)
+//         {
+//             if(nums[i] < nums[i-1])
+//                 return false;
+//         }
+//         return true;
     }
 };
