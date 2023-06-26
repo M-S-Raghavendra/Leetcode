4d874fa777301326b795d7ddf5ff7dfe783c3142
M-S-Raghavendra <99091196+M-S-Raghavendra@users.noreply.github.com>
2023-06-27 00:24:51 +0530

    Time: 95 ms (73.02%), Space: 18.7 MB (94.62%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -4,20 +4,22 @@ public:
         int m = matrix.size();
         int n = matrix[0].size();
         
-        vector<vector<int>> dp(m,vector<int>(n,0));
+        // vector<vector<int>> dp(m,vector<int>(n,0));
         int maxi = 0;
+        vector<int> prev(n,0), cur(n,0);
         
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
-                if(i == 0 or j == 0)
-                    dp[i][j] = matrix[i][j] - '0';
+                if(i == 0 or j == 0 or matrix[i][j] == '0')
+                    cur[j] = matrix[i][j] - '0';
                 else if(matrix[i][j] == '1')
-                    dp[i][j] = 1 + min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]));
+                    cur[j] = 1 + min(prev[j-1],min(prev[j],cur[j-1]));
                 // cout<<dp[i][j]<<" ";
-                maxi = max(maxi,dp[i][j]);
+                maxi = max(maxi,cur[j]);
             }
+            prev = cur;
             // cout<<endl;
         }
         return maxi*maxi;
