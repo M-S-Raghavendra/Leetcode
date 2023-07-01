4dfb5a5580ebeda1db12e67f0041c4747fb94694
M-S-Raghavendra <99091196+M-S-Raghavendra@users.noreply.github.com>
2023-07-01 23:09:31 +0530

    Time: 8 ms (83.67%), Space: 7 MB (76.94%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -2,50 +2,68 @@ class Solution {
 public:
     int characterReplacement(string s, int k) {
         int n = s.size();
-        int i = 0, j = 0;
-        int temp;
+        int i = 0;
+        // int temp;
         int ans = 0;
         
         int count[26] = {0};
-        for(auto x:s)
-            count[x-'A']++;
-        int mx = *max_element(count,count+26);
-        if(n-mx <= k)
-            return n;
+        int maxFreq = 0;
         
-        while(i < n and j < n)
+        for(int j=0;j<n;j++)
         {
-            temp = k;
-            while(j < n and (s[j] == s[i] or temp > 0))
-            {
-                if(s[j] != s[i]) temp--;
-                j++;
-            }
-            
-            ans = max(ans,(j-i));
-            i++;
-            while(i < n and s[i] == s[i-1])
+            count[s[j]-'A']++;
+            // maxFreq = max(maxFreq, count[s[j]-'A']);     // Trick to optimise O(26n) to O(n)
+            for(int k=0;k<26;k++)
+                maxFreq = max(maxFreq, count[k]);
+            if((j-i+1 - maxFreq) <= k)
+                ans = max(ans, j-i+1);
+            else {
+                count[s[i]-'A']--;
                 i++;
-            j = i;
-        }
-        
-        i = n-1, j = n-1;
-        while(i >= 0 and j >= 0)
-        {
-            temp = k;
-            while(j >= 0 and (s[j] == s[i] or temp > 0))
-            {
-                if(s[j] != s[i]) temp--;
-                j--;
             }
-            
-            ans = max(ans,(i-j));
-            i--;
-            while(i >= 0 and s[i] == s[i+1])
-                i--;
-            j = i;
         }
         
         return ans;
+        
+//         for(auto x:s)
+//             count[x-'A']++;
+//         int mx = *max_element(count,count+26);
+//         if(n-mx <= k)
+//             return n;
+        
+//         while(i < n and j < n)
+//         {
+//             temp = k;
+//             while(j < n and (s[j] == s[i] or temp > 0))
+//             {
+//                 if(s[j] != s[i]) temp--;
+//                 j++;
+//             }
+            
+//             ans = max(ans,(j-i));
+//             i++;
+//             while(i < n and s[i] == s[i-1])
+//                 i++;
+//             j = i;
+//         }
+        
+//         i = n-1, j = n-1;
+//         while(i >= 0 and j >= 0)
+//         {
+//             temp = k;
+//             while(j >= 0 and (s[j] == s[i] or temp > 0))
+//             {
+//                 if(s[j] != s[i]) temp--;
+//                 j--;
+//             }
+            
+//             ans = max(ans,(i-j));
+//             i--;
+//             while(i >= 0 and s[i] == s[i+1])
+//                 i--;
+//             j = i;
+//         }
+        
+//         return ans;
     }
 };
