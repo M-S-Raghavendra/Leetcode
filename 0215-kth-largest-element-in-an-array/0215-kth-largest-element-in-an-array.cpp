e14f9265536d2f7121d0cfeeef51c50be5653880
M-S-Raghavendra <99091196+M-S-Raghavendra@users.noreply.github.com>
2023-07-03 17:46:15 +0530

    Time: 180 ms (11.69%), Space: 45.4 MB (99.27%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,15 +1,57 @@
 class Solution {
 public:
-    int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int,vector<int>,greater<int>> pq;
+    int partition(vector<int> &nums, int l, int r)
+    {
+        int pivot = l;
+        int i = l;
+        int j = r;
         
-        for(auto x:nums)
+        while(1)
         {
-            pq.push(x);
-            if(pq.size() > k)
-                pq.pop();
+            while(i <= j and nums[i] <= nums[pivot])
+                i++;
+            while(i <= j and nums[j] > nums[pivot])
+                j--;
+            if(i < j)
+                swap(nums[i],nums[j]);
+            else {
+                swap(nums[pivot],nums[j]);
+                return j;
+            }
         }
         
-        return pq.top();
+        return 0;
+    }
+    
+    int quickSort(vector<int> &nums, int l, int r, int k)
+    {
+        int n = nums.size();
+        if(l <= r)
+        {
+            int s = partition(nums,l,r);
+            if(s == n-k)
+                return nums[s];
+            else if(s > n-k)
+                return quickSort(nums,l,s-1,k);
+            else
+                return quickSort(nums,s+1,r,k);
+        }
+        return 0;
+    }
+    
+    int findKthLargest(vector<int>& nums, int k) {
+        // Using Quixk sort PARTITION approach
+        return quickSort(nums,0,nums.size()-1,k);
+        
+//         priority_queue<int,vector<int>,greater<int>> pq;
+        
+//         for(auto x:nums)
+//         {
+//             pq.push(x);
+//             if(pq.size() > k)
+//                 pq.pop();
+//         }
+        
+//         return pq.top();
     }
 };
