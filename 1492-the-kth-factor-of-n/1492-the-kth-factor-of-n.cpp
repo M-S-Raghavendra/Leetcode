e61f062bb8a58643e7c8ced9e47cbe52199f80cd
M-S-Raghavendra <99091196+M-S-Raghavendra@users.noreply.github.com>
2023-07-10 12:59:39 +0530

    Time: 0 ms (100.00%), Space: 6 MB (35.60%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.cpp
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.cpp
@@ -3,16 +3,45 @@ public:
     int kthFactor(int n, int k) {
         int cnt = 0;
         
-        for(int i=1;i<=n;i++)
+        // O(sqrt(n))
+        for(int i=1;i*i<=n;i++)
+        {
+            if(n % i == 0)
+                cnt++;
+        }
+        
+        int size = 2*cnt;
+        double y = sqrt(1.0 * n);
+        int x = sqrt(n);
+        
+        if(x == y)
+            size--;
+        int ind = 0;
+        for(int i=1;i*i<=n;i++)
         {
             if(n % i == 0)
             {
-                cnt++;https://leetcode.com/problems/the-kth-factor-of-n/discuss/
-                if(cnt == k)
+                ind++;
+                if(ind == k)
                     return i;
+                if(size-ind+1 == k)
+                    return n/i;
             }
         }
         
-        return -1; 
+        return -1;
+        
+//         O(n)
+//         for(int i=1;i<=n;i++)
+//         {
+//             if(n % i == 0)
+//             {
+//                 cnt++;
+//                 if(cnt == k)
+//                     return i;
+//             }
+//         }
+        
+//         return -1; 
     }
 };
