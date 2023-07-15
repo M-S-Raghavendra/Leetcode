aa87e82687ef96107b8ed2cdde53dcf192251804
M-S-Raghavendra <99091196+M-S-Raghavendra@users.noreply.github.com>
2023-07-15 11:27:54 +0530

    Time: 351 ms (49.76%), Space: 99.1 MB (8.84%) - LeetHub

--- 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     int maxValue(vector<vector<int>>& events, int k) {
-        // unordered_map<int,int> m;
+        unordered_map<int,int> m;
         int n = events.size();
         sort(events.begin(),events.end());
         
-        // for(int i=0;i<n;i++)
-        // {
-        //     int ind = -1;
-        //     for(int j=i+1;j<n;j++)
-        //     {
-        //         if(events[i][1] < events[j][0])
-        //         {
-        //             ind = j;
-        //             break;
-        //         }
-        //     }
-        //     if(ind == -1)
-        //         ind = n;
-        //     events[i].push_back(ind);
-        // }
+        for(int i=0;i<n;i++)
+        {
+            // int ind = -1;
+            for(int j=i+1;j<n;j++)
+            {
+                if(events[i][1] < events[j][0])
+                {
+                    m[i] = j;
+                    break;
+                }
+            }
+            if(m.find(i) == m.end())
+                m[i] = n;
+        }
         
 //         for(auto x:m)
 //             cout<<x.first<<" "<<x.second<<endl;
@@ -27,10 +26,10 @@ public:
 //             cout<<x[0]<<" "<<x[1]<<" "<<x[2]<<endl;
         
         vector<vector<int>> dp(n,vector<int>(k+1,-1));
-        return f(0,k,events,dp);
+        return f(0,k,events,m,dp);
     }
     
-    int f(int i, int k, vector<vector<int>> &e, vector<vector<int>> &dp)
+    int f(int i, int k, vector<vector<int>> &e, unordered_map<int,int> &m, vector<vector<int>> &dp)
     {
         if(i == e.size())
             return 0;
@@ -39,9 +38,9 @@ public:
         
         if(dp[i][k] != -1) return dp[i][k];
         
-        int notpick = 0 + f(i+1,k,e,dp);
-        int next = bsearch(e,i,i+1,e.size()-1);
-        int pick = e[i][2] + f(next,k-1,e,dp);
+        int notpick = 0 + f(i+1,k,e,m,dp);
+        // int next = bsearch(e,i,i+1,e.size()-1);
+        int pick = e[i][2] + f(m[i],k-1,e,m,dp);
         // cout<<i<<" "<<notpick<<" "<<pick<<endl;
         
         return dp[i][k] = max(pick,notpick);
