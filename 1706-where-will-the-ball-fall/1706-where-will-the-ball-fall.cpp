5da09ffd2e5754b97ed476739659175524f90db0
M-S-Raghavendra <99091196+M-S-Raghavendra@users.noreply.github.com>
2023-07-28 04:39:59 +0000

    Time: 26 ms (73.35%), Space: 14.4 MB (5.75%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -4,14 +4,39 @@ public:
         int m = grid.size();
         int n = grid[0].size();
         vector<int> answer(n);
-        vector<vector<int>> dp(m,vector<int>(n,-1));
+        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
         
-        for(int col = 0; col < n; col++)
+        // for(int col = 0; col < n; col++)
+        // {
+        //     answer[col] = f(0,col,grid,dp);
+        // }
+        
+        for(int i=m;i>=0;i--)
         {
-            answer[col] = f(0,col,grid,dp);
+            for(int j=n-1;j>=0;j--)
+            {
+                if(i == m) dp[i][j] = j;
+                else {
+                    if(grid[i][j] == 1)
+                    {
+                        if(j+1 < grid[0].size() and grid[i][j+1] == 1)
+                            dp[i][j] = dp[i+1][j+1];
+                        else
+                            dp[i][j] = -1;
+                    }
+                    else {
+                        if(j-1 >= 0 and grid[i][j-1] == -1)
+                            dp[i][j] = dp[i+1][j-1];
+                        else
+                            dp[i][j] = -1;
+                    }
+                }
+            }
         }
-        
-        return answer;
+        vector<int> ans = dp[0];
+        ans.pop_back();
+        return ans;
+        // return answer;
     }
     
     int f(int i, int j, vector<vector<int>> &grid, vector<vector<int>> &dp)
