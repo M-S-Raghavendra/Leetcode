e4f5d55601eee68d57642283f7b0c070877e29d2
M-S-Raghavendra <99091196+M-S-Raghavendra@users.noreply.github.com>
2023-07-28 05:26:15 +0000

    Time: 6 ms (52.86%), Space: 8.6 MB (8.42%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -3,22 +3,25 @@ public:
     bool PredictTheWinner(vector<int>& nums) {
         int n = nums.size();
         int total = accumulate(nums.begin(),nums.end(),0);
+        vector<vector<vector<int>>> dp(n,vector<vector<int>>(n,vector<int>(2,-1)));
         
-        int p1 = f(0,n-1,0,nums);
+        int p1 = f(0,n-1,0,nums,dp);
         int p2 = total - p1;
-        cout<<p1<<endl;
+        // cout<<p1<<endl;
         return p1 >= p2;
     }
     
-    int f(int i, int j, int turn, vector<int> &nums)\
+    int f(int i, int j, int turn, vector<int> &nums, vector<vector<vector<int>>> &dp)
     {
         if(i > j)
             return 0;
         // cout<<i<<" "<<j<<" "<<turn<<endl;
         
+        if(dp[i][j][turn] != -1) return dp[i][j][turn];
+        
         if(turn == 0)
-            return max(nums[i] + f(i+1,j,1,nums), nums[j] + f(i,j-1,1,nums));
+            return dp[i][j][turn] = max(nums[i] + f(i+1,j,1,nums,dp), nums[j] + f(i,j-1,1,nums,dp));
         else
-            return min(f(i+1,j,0,nums), f(i,j-1,0,nums));
+            return dp[i][j][turn] = min(f(i+1,j,0,nums,dp), f(i,j-1,0,nums,dp));
     }
 };
