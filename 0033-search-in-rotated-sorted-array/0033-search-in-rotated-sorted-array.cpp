614c87e9bb0b9c62f2302ae173830d7289a778c9
M-S-Raghavendra <99091196+M-S-Raghavendra@users.noreply.github.com>
2023-08-08 08:08:59 +0000

    Time: 0 ms (100.00%), Space: 11.1 MB (45.03%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -9,26 +9,18 @@ public:
             int m = (s+e)/2;
             if(nums[m] == target)
                 return m;
-            if(nums[m] >= nums[s])
+            if(nums[s] <= nums[m])
             {
-                if(target > nums[m])
+                if(nums[s] <= target and target <= nums[m])
+                    e = m-1;
+                else
                     s = m+1;
-                else {
-                    if(target >= nums[s])
-                        e = m;
-                    else
-                        s = m+1;
-                }
             }
             else {
-                if(target < nums[m])
-                    e = m;
-                else {
-                    if(target < nums[s])
-                        s = m+1;
-                    else
-                        e = m;
-                }
+                if(nums[m] <= target and target <= nums[e])
+                    s = m+1;
+                else
+                    e = m-1;
             }
         }
         
