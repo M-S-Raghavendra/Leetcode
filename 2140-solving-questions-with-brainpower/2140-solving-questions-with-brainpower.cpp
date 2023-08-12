85f4b4fe1136ae5cf408f7acdb039144afb81813
M-S-Raghavendra <99091196+M-S-Raghavendra@users.noreply.github.com>
2023-08-12 16:04:00 +0000

    Time: 278 ms (88.80%), Space: 121.5 MB (47.62%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -1,38 +1,22 @@
 class Solution {
-private:
-    long long f(int i, vector<vector<int>> &q, vector<long long> &dp)
+public:
+    vector<long long> dp;
+    
+    long long mostPoints(vector<vector<int>>& questions) {
+        dp.resize(questions.size(),-1);
+        return f(0,questions);
+    }
+    
+    long long f(int i, vector<vector<int>> &q)
     {
         if(i >= q.size())
             return 0;
         
         if(dp[i] != -1) return dp[i];
         
-        long long notpick = 0 + f(i+1,q,dp);
-        long long pick = q[i][0] + f(i+q[i][1]+1,q,dp);
+        long long np = 0 + f(i+1,q);
+        long long p = q[i][0] + f(i+q[i][1]+1, q);
         
-        return dp[i] = max(notpick,pick);
-    }
-    
-public:
-    long long mostPoints(vector<vector<int>>& q) {
-        long long ans;
-        int n = q.size();
-        vector<long long> dp(n,-1);
-        
-        // ans = f(0,q,dp);
-        
-        dp[n-1] = q[n-1][0];
-        
-        for(int i=n-2;i>=0;i--)
-        {
-            long long notpick = 0, pick = q[i][0];
-            notpick = 0 + dp[i+1];
-            if(i+q[i][1]+1 < n)
-                pick += dp[i+q[i][1]+1];
-
-            dp[i] = max(notpick,pick);
-        }
-        
-        return dp[0];
+        return dp[i] = max(np,p);
     }
 };
