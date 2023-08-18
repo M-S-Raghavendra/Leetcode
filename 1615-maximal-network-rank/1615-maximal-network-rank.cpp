9bdb1728d6a76debdea7efbeb7f2badf49510467
M-S-Raghavendra <99091196+M-S-Raghavendra@users.noreply.github.com>
2023-08-18 12:05:11 +0530

    Time: 67 ms (87.49%), Space: 33.4 MB (55.73%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.cpp
+++ 1615-maximal-network-rank/1615-maximal-network-rank.cpp
@@ -4,24 +4,22 @@ public:
         set<pair<int,int>> s;
         vector<int> deg(n);
         
-        for(auto x:roads)
+        for(auto &x:roads)
         {
             deg[x[0]]++;
             deg[x[1]]++;
             s.insert({x[0],x[1]});
         }
         
-        for(auto x:deg)
-            cout<<x<<" ";
-        
         int maxi = INT_MIN;
+        int cur;
         
         for(int i=0;i<n;i++)
         {
             for(int j=i+1;j<n;j++)
             {
-                int cur = deg[i] + deg[j];
-                if(s.find({i,j}) != s.end() or s.find({j,i}) != s.end())
+                cur = deg[i] + deg[j];
+                if(s.count({i,j}) or s.count({j,i}))
                     cur--;
                 maxi = max(maxi,cur);
             }
