84b0cc9c3274585f9400eb909bcd5c95d60f9737
M-S-Raghavendra <99091196+M-S-Raghavendra@users.noreply.github.com>
2023-08-22 11:27:27 +0530

    Time: 2 ms (50.67%), Space: 5.9 MB (62.61%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t ans = 0;
+        int i = 31;
+        
+        while(n)
+        {
+            ans = (ans | ((n%2)<<i));
+            i--;
+            n /= 2;
+        }
+        
+        return ans;
+    }
+};
