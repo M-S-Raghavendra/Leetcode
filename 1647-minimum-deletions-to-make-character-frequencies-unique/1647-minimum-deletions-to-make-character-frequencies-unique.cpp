58a7c95c1699674ef90f5f2f78808effc95bd4cd
M-S-Raghavendra <99091196+M-S-Raghavendra@users.noreply.github.com>
2023-09-14 10:11:56 +0530

    Time: 80 ms (29.26%), Space: 17.8 MB (8.01%) - LeetHub

---
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int minDeletions(string s) {
+        int n = s.size();
+        vector<int> count(26,0);
+        unordered_map<int,int> m;
+        int ans = 0;
+        
+        for(auto x:s)
+            count[x-'a']++;
+        for(int i=0;i<26;i++)
+        {
+            cout<<(char)(i+'a')<<" "<<count[i]<<endl;
+        }
+        cout<<endl;
+        for(int i=0;i<26;i++)
+        {
+            if(count[i] != 0)
+                m[count[i]]++;
+        }
+        
+        for(auto x:m)
+            cout<<x.first<<" "<<x.second<<endl;
+        
+        for(int i=0;i<n;i++)
+        {
+            int cnt = count[s[i]-'a'];
+            while(m[cnt] > 1)
+            {
+                m[cnt]--;
+                ans++;
+                // cout<<s[i]<<" "<<i<<" "<<endl;
+                count[s[i]-'a']--;
+                cnt--;
+                if(cnt == 0) break;
+                m[cnt]++;
+            }
+        }
+        
+        return ans;
+    }
+};
